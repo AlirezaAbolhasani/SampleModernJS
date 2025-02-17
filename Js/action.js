b1f3ab00f8044f093780c7c6dcd04ad58ba92407
AlirezaAbolhasani <alireza.ab.1111@gmail.com>
2025-02-17 16:01:07 +0330

    v2

--- Js/action.js
+++ Js/action.js
@@ -1,3 +1,33 @@
-const desc_ = document.querySelector('@desc');
-console.log(desc_);
-console.log(adfafdafafaf);
+const formExpense = document.querySelector("#formExpense");
+const formDesc = document.querySelector("#desc");
+const formAmountInput = document.querySelector("#inputAmount");
+const frmTotalAmnt = document.querySelector("#totalAmnt")
+
+//Array
+const totalAmntArr = [];
+
+formExpense.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const desc =  formDesc.value.trim();
+    const amount = formAmountInput.value.trim();
+
+    console.log("Desc = ",desc ,'Amount =' , amount);
+
+    totalAmntArr.push({desc,amount});
+    formAmountInput.value ="";
+    formDesc.value = "";
+    addToArray();
+
+});
+
+const addToArray = ()=>{
+    
+    let totalAmnt_ = 0 ;
+
+    for( let i = 0 ; i < totalAmntArr.length; i++){
+        totalAmnt_ += parseFloat(totalAmntArr[i].amount);
+    }
+
+    frmTotalAmnt.textContent = totalAmnt_.toLocaleString("fa-IR");
+
+}
