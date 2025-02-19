dbb1200150a2a74d329655d36f64243becb7b1f2
AlirezaAbolhasani <alireza.ab.1111@gmail.com>
2025-02-19 18:43:05 +0330

    14031201-v5

--- Js/action.js
+++ Js/action.js
@@ -1,3 +1,18 @@
+
+//Anonymous Function
+function changingPageColor(Template) {
+    document.querySelector('html').setAttribute('data-theme', Template);
+    if (Template === 'dark') {
+        document.querySelector('#dark-btn').style.display = "none";
+        document.querySelector('#light-btn').style.display = "block"; // نمایش دکمه light		
+
+    } else {
+        document.querySelector('#light-btn').style.display = "none";
+        document.querySelector('#dark-btn').style.display = "block"; // نمایش دکمه dark
+    }
+}
+
+
 const formExpense = document.querySelector("#formExpense");
 const formDesc = document.querySelector("#desc");
 const formAmountInput = document.querySelector("#inputAmount");
@@ -47,70 +62,4 @@ const showInPage = ()=>{
 const removeExpense=(index)=> {
     totalAmntArr = totalAmntArr.filter(( _ , i ) => i !== index);
     showInPage();
-};
-
-import { mkdir, readFile, writeFile } from 'node:fs/promises';
-import { iconToSVG, iconToHTML, parseIconSetAsync } from '@iconify/utils';
-import { locate } from '@iconify/json';
-
-/**
- * Dimensions of generated SVG:
- * - '1em' -> 1em, easy to resize icons with font-size.
- * - 'auto' -> same as icon's viewBox.
- * - 'unset' -> no width/height in generated icons. You'll need to assign width and height in CSS.
- */
-const height = '1em';
-
-/**
- * List of icon sets you want to export
- *
- * @type {string[]}
- */
-const prefixes = ['mdi', 'mdi-light'];
-
-/**
- * Output directory for SVG
- */
-const target = 'assets/svg';
-
-// Parse each icon set
-for (let i = 0; i < prefixes.length; i++) {
-	const prefix = prefixes[i];
-
-	// Find location of .json file
-	const filename = locate(prefix);
-
-	// Load file and parse it
-	/** @type {import("@iconify/types").IconifyJSON} */
-	const iconSet = JSON.parse(await readFile(filename, 'utf8'));
-
-	// Create output directory if it doesn't exist
-	const outDir = `${target}/${prefix}`;
-	try {
-		await mkdir(outDir, {
-			recursive: true,
-		});
-	} catch {}
-
-	// Get all icons
-	let counter = 0;
-	await parseIconSetAsync(iconSet, async (name, data) => {
-		if (!data) {
-			// Failed icon
-			return;
-		}
-
-		// Generate SVG
-		const { attributes, body } = iconToSVG(data, {
-			height,
-		});
-		const svg = iconToHTML(body, attributes);
-
-		// Save it
-		await writeFile(`${outDir}/${name}.svg`, svg, 'utf8');
-		counter++;
-	});
-
-	// Log it
-	console.log(`Exported ${counter} icons from ${iconSet.info?.name || prefix}`);
 }
